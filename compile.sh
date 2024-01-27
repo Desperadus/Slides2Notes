7f923823ad2e2e5a33ab1d00a0320682ff33d6ea
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2024-01-27 14:13:46 +0100

    minor refinement

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 pandoc output.md -o output.tex --standalone
 pdflatex -interaction=nonstopmode -pdf output.tex
--- prompts.py
+++ prompts.py
@@ -2,15 +2,18 @@ basic_cz = """Na vstupu dostaneš slidy z prezentací k mému předmětu. Tvým 
 Na slidech budou nějaké odrážky textu atd. Napiš k nim co nejvíce víš o daných věcech a co je k tomu dále relevantní.
 Piš co nejvíce textu. Výstupem má být markdown. Pokud je matematika na slidech, tak ji napiš v LaTeXu.
 """
+
 new_image_prompt_cz = """Na vstupu dostaneš slidy z prezentací k mému předmětu. Tvým úkolem je napsat velice podrobná skripta. 
 Na slidech budou nějaké odrážky textu atd. Napiš k nim co nejvíce víš o daných věcech a co je k tomu dále relevantní.
 Piš co nejvíce textu. Výstupem má být markdown. Pokud je matematika na slidech, tak ji napiš v LaTeXu.
 """
+
 basic_eng = """You will get slide from my lectures. Your task is to write detailed notes for examination.
-There will be some bullet points of text on the slides etc. Write as much text as possible about the points.
-The output should be markdown. If there is mathematics on the slides, write it in LaTeX.
+There will be some bullet points of text on the slides etc. Write as much text as possible about the points (if possible). Dont write conlusions.
+The output should be markdown. If there is mathematics on the slides or special symbols, write it in LaTeX.
 """
+
 new_image_prompt_eng = """You will get slide from my lectures. Your task is to write detailed notes for examination.
-There will be some bullet points of text on the slides etc. Write as much text as possible about the points.
-The output should be markdown. If there is mathematics on the slides, write it in LaTeX.
+There will be some bullet points of text on the slides etc. Write as much text as possible about the points. Dont write conlusions.
+The output should be markdown. If there is mathematics on the slides or special symbols, write it in LaTeX.
 """
