2a5fbf131979f6b2a528a19cde09b05b2549ff45
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2024-01-27 12:51:12 +0100

    update a little bit

--- prompts.py
+++ prompts.py
@@ -2,4 +2,15 @@ basic_cz = """Na vstupu dostaneš slidy z prezentací k mému předmětu. Tvým 
 Na slidech budou nějaké odrážky textu atd. Napiš k nim co nejvíce víš o daných věcech a co je k tomu dále relevantní.
 Piš co nejvíce textu. Výstupem má být markdown. Pokud je matematika na slidech, tak ji napiš v LaTeXu.
 """
-new_image_prompt_cz = """Napiš co nejvíce textu k tomuto obrázku. Výstupem má být markdown. Pokud je matematika na slidech, tak ji napiš v LaTeXu. K minulým slidům jsi napsal toto (neopakuj věci zbytečně):"""
+new_image_prompt_cz = """Na vstupu dostaneš slidy z prezentací k mému předmětu. Tvým úkolem je napsat velice podrobná skripta. 
+Na slidech budou nějaké odrážky textu atd. Napiš k nim co nejvíce víš o daných věcech a co je k tomu dále relevantní.
+Piš co nejvíce textu. Výstupem má být markdown. Pokud je matematika na slidech, tak ji napiš v LaTeXu.
+"""
+basic_eng = """You will get slides from my lectures. Your task is to write very detailed scripts.
+There will be some bullet points of text on the slides etc. Write as much text as possible about the points.
+The output should be markdown. If there is mathematics on the slides, write it in LaTeX.
+"""
+new_image_prompt_eng = """You will get slides from my lectures. Your task is to write very detailed scripts.
+There will be some bullet points of text on the slides etc. Write as much text as possible about the points.
+The output should be markdown. If there is mathematics on the slides, write it in LaTeX.
+"""
