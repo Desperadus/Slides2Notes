5cc28eb86e5050b4ba4d1932c37f3793ad6143c9
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2024-01-27 13:51:56 +0100

    rename file

--- img_to_md.py
+++ img_to_md.py
@@ -5,6 +5,7 @@ import argparse
 import PIL.Image
 from tqdm import tqdm
 import prompts
+import concurrent.futures
 
 VERBOSE = False
 
@@ -22,15 +23,35 @@ def load_model():
 def get_text_from_image(model, png_slides, intro_prompt=prompts.basic_eng, new_image_prompt=prompts.new_image_prompt_eng):
     # chat = model.start_chat(history=[])
     results = []
-    response = model.generate_content([intro_prompt, PIL.Image.open(png_slides[2])])
+    response = model.generate_content([intro_prompt, PIL.Image.open(png_slides[0])])
     results.append(response.text)
     printing(response.text)
 
-    for slide in tqdm(png_slides[3:10]):
+    for slide in tqdm(png_slides[1:]):
         image = PIL.Image.open(slide)
         response = model.generate_content([new_image_prompt,image])
         results.append(response.text)
         printing(response.text)
+    return results
+
+def process_slide(slide, progress, new_image_prompt=prompts.new_image_prompt_eng):
+    text = ""
+    image = PIL.Image.open(slide)
+    response = model.generate_content([new_image_prompt, image])
+    for part in response.parts:
+        text += part.text
+        printing(part.text)
+    progress.update()
+    return text
+
+def get_text_from_image_paralel(model, png_slides, intro_prompt=prompts.basic_eng, new_image_prompt=prompts.new_image_prompt_eng):
+    results = []
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        with tqdm(total=len(png_slides)) as progress:
+            for slide, result in zip(png_slides, executor.map(lambda slide: process_slide(slide, progress), png_slides)):
+                results.append(result)
+
     return results
 
 
@@ -48,8 +69,8 @@ if __name__ == "__main__":
 
 
     model = load_model()
-    results = get_text_from_image(model, img_paths)
-    result = "\n".join(results)
-    result = result.replace(" #", "#")
+    results = get_text_from_image_paralel(model, img_paths)
+    result = "\n\n".join(results)
+    result = result.replace(" #", "#")#.replace("\n---\n", "\n")
     with open(args.output, "w") as f:
         f.write(result)
